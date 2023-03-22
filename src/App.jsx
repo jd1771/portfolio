9a439766b9f0d6022d1b60b13d82c1c89b35bd0a
Jas Dhaul <jasdhaul0@gmail.com>
2023-03-21 20:35:03 -0700

    Implemented footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="w-100 mt-10 flex h-[500px] flex-col items-center justify-around bg-[#191B1B] text-white md:flex-row">
+    <div className="w-100 mt-10 flex h-[700px] flex-col items-center justify-around bg-[#191B1B] text-white md:h-[600px] md:flex-row">
       <div className="mt-5 flex flex-col text-left">
         <h1 className="text text-left text-3xl font-bold text-cyan">
           Let's get in touch

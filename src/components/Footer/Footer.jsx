96c91d2eb1f160bbf850049212d17241097df344
Jas Dhaul <jasdhaul0@gmail.com>
2023-04-01 16:51:37 -0700

    init commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/Bs";
+import { AiFillGithub } from "react-icons/Ai";
+
+const Footer = () => {
+  return (
+    <div className="flex h-[300px] w-full flex-col items-center justify-center gap-y-4 text-white">
+      <p className="text-md">Made with ❤️ by Jas Dhaul</p>
+      <div className="flex items-center justify-center gap-x-5">
+        <a
+          href="https://www.linkedin.com/in/jas-dhaul-90b0181aa/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin size={30} />
+        </a>
+        <a href="https://github.com/jd1771" target="_blank" rel="noreferrer">
+          <AiFillGithub size={30} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Projects/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dzl29lmyv/image/upload/v1680388958/1_zrmmkh.jpg",
+    title: "AI-Imagery",
+    description:
+      "Unleash your creativity and generate unique AI-generated images on AI Imagery. Display your creations on our homepage alongside other user-generated AI images in our gallery. Join our community today and start exploring the possibilities of AI-generated imagery!",
+    github: "https://github.com/jd1771/AI-Imagery",
+    deployment: "https://jd1771.github.io/AI-Imagery/",
+  },
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    title: "ML-Artwork",
+    description:
+      "ML-Artwork is a project explores the use of neural networks in predicting the artists behind famous paintings. With neural networks, you can gain a deeper understanding of the world of art and explore the fascinating intersection between technology and the arts.",
+    github: "https://github.com/jd1771/ML-Artwork",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dzl29lmyv/image/upload/v1680388958/Discord_and_Ethereum_drdNBC.jpg_yvs1xs.webp",
+    title: "Crypto-Notifier",
+    description:
+      "A Discord bot that makes it easy for users to set price alerts for their favorite cryptocurrencies. With this server bot, you can choose from a wide variety of cryptocurrencies and set custom alerts for specific price points.",
+    github: "https://github.com/jd1771/crypto-notifier",
+  },
+];
